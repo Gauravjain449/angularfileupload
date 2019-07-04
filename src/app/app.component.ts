ee22096a2f2f2c26a91bc7058762bf35cb74dda3
Gauravjain449 <g9717811255@gmail.com>
2019-07-04 23:01:03 +0530

    ll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+import {HttpClient} from '@angular/common/http';
+import {MatDialog, MatPaginator, MatSort} from '@angular/material';
+
+import {DataSource} from '@angular/cdk/collections';
+
+import {BehaviorSubject, fromEvent, merge, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'file-upload-client-side';
+
+export class AppComponent implements OnInit {
+  displayedColumns = [];
+  exampleDatabase:  null;
+  dataSource:  null;
+  index: number;
+  id: number;
+
+  constructor() {}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('filter') filter: ElementRef;
+
+  ngOnInit() {
+   // this.loadData();
+  }
+
+  refresh() {
+   // this.loadData();
+  }
+    public loadData() {
+    this.exampleDatabase = null;
+    this.dataSource = null;
+    
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {
+  MatButtonModule, MatDialogModule, MatIconModule, MatInputModule, MatPaginatorModule, MatSortModule,
+  MatTableModule, MatToolbarModule,
+} from '@angular/material';
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatDialogModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  entryComponents: [
+  
+  ],
+  providers: [
+  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
